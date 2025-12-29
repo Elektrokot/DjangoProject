65ab70a06da7cc7cd328c31814eeaa1e8c616f98
Elektrokot <markus.rulit@gmail.com>
2025-12-29 23:13:32 +1000

    init commit

---
+++ students/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from students import views
+
+app_name = 'students'
+
+
+urlpatterns = [
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+]
